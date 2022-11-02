d1a442df2292e58506ac7e9d568c3289d2013c0d
TBertrand_arkos <bertrand.tarascon@epita.fr>
2022-11-02 19:52:58 +0100

    [ADD][USER] Possible to create a user

--- user.py
+++ user.py
@@ -1,8 +1,30 @@
 from PyInquirer import prompt
 user_questions = [
-
+    {
+        "type":"input",
+        "name":"name",
+        "message":"User creation - name: ",
+    },
+    {
+        "type":"input",
+        "name":"email",
+        "message":"User creation - email: ",
+    },
+    {
+        "type":"input",
+        "name":"iban",
+        "message":"User creation - IBAN: ",
+    },
 ]
 
 def add_user():
-    # This function should create a new user, asking for its name
-    return
+    infos = prompt(user_questions)
+    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    name = infos['name']
+    email = infos['email']
+    iban = infos['iban']
+    # put into csv file
+    with open('users.csv', 'a') as f:
+        f.write(f'{name},{email},{iban}')
+    print("User Added !")
+    return True
